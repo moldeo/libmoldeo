c370f334b3a4d6256a31de793f99f815843a3b43
fabricio_costa <fabricio_costa@c963c68c-f84c-0410-a3d6-bef16c67260e>
2013-08-21 14:34:07 +0000

    added libmoldeotest for linking tests

---
+++ libmoldeo/trunk/libmoldeotest/moldeotest.h
@@ -0,0 +1,51 @@
+#ifndef MO_MOLDEO_HEADERS
+#define MO_MOLDEO_HEADERS
+
+#include "moTypes.h"
+//#include "moConsole.h"
+
+/**
+*   \mainpage
+*   \if spanish
+*
+*   \image html MoldeoHeaderLogoTransicionTierra2.png "Moldeo     http://www.moldeo.org"
+*
+*   Bienvenidos a la documentación de Moldeo, una plataforma de desarrollo para aplicaciones multimediales interactivas en tiempo real.
+*
+*   \code
+
+    #include "moldeo.h"
+
+    void main() {
+
+        moConsole MoldeoConsole;
+
+        // Inicializamos en primer lugar
+        if ( MoldeoConsole.Init( moText("test"),
+                                moText("test.mol") ) ) {
+
+            // Ciclo principal
+            // Mientras interactuamos
+            while( MoldeoConsole.Interaction() ) {
+
+                //Actualizamos
+                MoldeoConsole.Update();
+
+                //Dibujamos
+                MoldeoConsole.Draw();
+            }
+         }
+    }
+*   \endcode
+*
+*
+*
+*   \author     Moldeo Interactive
+*   \author
+*   \author     Fabricio Costa Alisedo
+*   \author
+*   \endif
+*
+*/
+
+#endif
