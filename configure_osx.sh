d002430df6f675272e73621a5e6700c1e514881d
ArtCenter/South Florida <artcentersouthflorida@ArtCenterSouths-Mac-mini.local>
2016-02-15 05:14:31 -0500

    Added gstreamer 1.0 autoconf

--- configure_osx.sh
+++ configure_osx.sh
@@ -1,1 +1,1 @@
-./autogen_macports.sh --enable-debug --prefix=/opt/local --libdir=/opt/local/lib --enable-host=osx CPPFLAGS=-I/opt/local/include LDFLAGS=-L/opt/local/lib
+./autogen_macports.sh --enable-debug --prefix=/opt/local --libdir=/opt/local/lib --enable-host=osx CPPFLAGS="-I/opt/local/include -DGSTVERSION" LDFLAGS=-L/opt/local/lib
